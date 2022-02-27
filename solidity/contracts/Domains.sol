002803a1dd0231cc753156387614f36a1e1831ad
gareth-was-here <gareth@garethveale.dev>
2022-02-27 14:25:16 +0000

    addt'l functions on domain contract + call from run script

--- solidity/contracts/Domains.sol
+++ solidity/contracts/Domains.sol
@@ -7,17 +7,31 @@ import "hardhat/console.sol";
 contract Domains {
 
     mapping(string => address) public domains;
+    mapping(string => string) public records;
+
 
     constructor() {
         console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
     }
 
     function register(string calldata name) public {
+        // check name is unregistered - address(0) is basically the null address value
+        require(domains[name] == address(0));
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
 
     function getAddress(string calldata name) public view returns (address) {
         return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        // check domain owner is txn sender
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns(string memory) {
+        return records[name];
     }
 }
