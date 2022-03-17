8158d7966dad0094beb42a1e05d0bff44f41cd64
gareth-was-here <gareth@garethveale.dev>
2022-03-17 11:37:11 +0000

    price function + mint svg nft on registering domain

--- solidity/scripts/run.js
+++ solidity/scripts/run.js
@@ -1,20 +1,19 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("ninja");
     await domainContract.deployed();
+    
     console.log("Contract deployed to:", domainContract.address);
-    console.log("Contract deployed by:", owner.address);
-
-    const txn = await domainContract.register("doom");
+    
+    const txn = await domainContract.register("doom", {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
     const domainAddress = await domainContract.getAddress("doom");
     console.log("Owner of domain doom:", domainAddress);
 
-    // try to set a record that doesn't belong to address
-    txn = await domainContract.connect(randomPerson).setRecord("doom", "haha my domain now!");
-    await txn.wait();
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 };
   
 const runMain = async () => {
